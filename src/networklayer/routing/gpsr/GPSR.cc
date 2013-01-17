d9846ab7fe698c124aa0475e09f2298753f62eb5
Levente Meszaros <levy@valarmorghulis.(none)>
2013-01-17 18:10:38 +0100

    GPSR: first successful ping

--- src/networklayer/routing/gpsr/GPSR.cc
+++ src/networklayer/routing/gpsr/GPSR.cc
@@ -1,20 +1,42 @@
 //
-// This program is property of its copyright holder. All rights reserved.
-// 
+// Copyright (C) 2004 Andras Varga
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
 
 #include "GPSR.h"
+#include "NotificationBoard.h"
+#include "InterfaceTableAccess.h"
+#include "IPProtocolId_m.h"
 
 Define_Module(GPSR);
 
 #define GPSR_EV EV << "GPSR at " << getHostName() << " "
 
+// KLUDGE:
+PositionTable GPSR::globalPositionTable;
+
 GPSR::GPSR() {
     notificationBoard = NULL;
     addressPolicy = NULL;
-    globalLocationTable = NULL;
+    beaconTimer = NULL;
 }
 
 GPSR::~GPSR() {
+    cancelAndDelete(beaconTimer);
+    cancelAndDelete(purgeNeighborsTimer);
 }
 
 //
@@ -24,13 +46,40 @@ GPSR::~GPSR() {
 void GPSR::initialize(int stage) {
     if (stage == 0) {
         // context parameters
+        routingTableModuleName = par("routingTableModuleName");
         networkProtocolModuleName = par("networkProtocolModuleName");
+        // gpsr parameters
+        interfaces = par("interfaces");
+        beaconInterval = par("beaconInterval");
+        maxJitter = par("maxJitter");
+        neighborValidityInterval = par("neighborValidityInterval");
         // context
+        notificationBoard = NotificationBoardAccess().get(this);
+        interfaceTable = InterfaceTableAccess().get(this);
+        mobility = check_and_cast<IMobility *>(findModuleWhereverInNode("mobility", this));
+        // KLUDGE: simplify this when IPv4RoutingTable implements IRoutingTable
+        routingTable = check_and_cast<IRoutingTable *>(findModuleWhereverInNode(routingTableModuleName, this));
         networkProtocol = check_and_cast<INetfilter *>(findModuleWhereverInNode(networkProtocolModuleName, this));
+        // internal
+        beaconTimer = new cMessage("BeaconTimer");
+        purgeNeighborsTimer = new cMessage("PurgeNeighborsTimer");
+        scheduleBeaconTimer();
+        schedulePurgeNeighborsTimer();
     }
     else if (stage == 4) {
+        notificationBoard->subscribe(this, NF_LINK_BREAK);
         addressPolicy = getSelfAddress().getAddressPolicy();
-        // hook to network protocol
+        // join multicast groups
+        cPatternMatcher interfaceMatcher(interfaces, false, true, false);
+        for (int i = 0; i < interfaceTable->getNumInterfaces(); i++) {
+            InterfaceEntry * interfaceEntry = interfaceTable->getInterface(i);
+            if (interfaceEntry->isMulticast() && interfaceMatcher.matches(interfaceEntry->getName()))
+                // Most AODVv2 messages are sent with the IP destination address set to the link-local
+                // multicast address LL-MANET-Routers [RFC5498] unless otherwise specified. Therefore,
+                // all AODVv2 routers MUST subscribe to LL-MANET-Routers [RFC5498] to receiving AODVv2 messages.
+                addressPolicy->joinMulticastGroup(interfaceEntry, addressPolicy->getLinkLocalManetRoutersMulticastAddress());
+        }
+        // hook to netfilter
         networkProtocol->registerHook(0, this);
     }
 }
@@ -47,27 +96,139 @@ void GPSR::handleMessage(cMessage * message) {
 //
 
 void GPSR::processSelfMessage(cMessage * message) {
-    throw cRuntimeError("Unknown self message");
+    if (message == beaconTimer)
+        processBeaconTimer();
+    else if (message == purgeNeighborsTimer)
+        processPurgeNeighborsTimer();
+    else
+        throw cRuntimeError("Unknown self message");
 }
 
 void GPSR::processMessage(cMessage * message) {
-    throw cRuntimeError("Unknown message");
+    if (dynamic_cast<UDPPacket *>(message))
+        processUDPPacket((UDPPacket *)message);
+    else
+        throw cRuntimeError("Unknown message");
 }
 
 //
-// handling positions
+// beacon timers
 //
 
-Coord GPSR::getLocation(const Address & address) {
-    return globalLocationTable->getLocation(address);
+void GPSR::scheduleBeaconTimer() {
+    GPSR_EV << "Scheduling beacon timer" << endl;
+    scheduleAt(simTime() + beaconInterval, beaconTimer);
 }
 
-Coord GPSR::findNextHop(const Address & destination) {
-    return Coord();
+void GPSR::processBeaconTimer() {
+    GPSR_EV << "Processing beacon timer" << endl;
+    sendBeaconPacket(createBeaconPacket(), uniform(0, maxJitter).dbl());
+    scheduleBeaconTimer();
+    schedulePurgeNeighborsTimer();
+    // KLUDGE:
+    globalPositionTable.setPosition(getSelfAddress(), mobility->getCurrentPosition());
 }
 
 //
-// handling addresses
+// handling purge neighbors timers
+//
+
+void GPSR::schedulePurgeNeighborsTimer() {
+    GPSR_EV << "Scheduling purge neighbors timer" << endl;
+    simtime_t nextExpiration = getNextNeighborExpiration();
+    if (nextExpiration == SimTime::getMaxTime()) {
+        if (purgeNeighborsTimer->isScheduled())
+            cancelEvent(purgeNeighborsTimer);
+    }
+    else {
+        if (!purgeNeighborsTimer->isScheduled())
+            scheduleAt(nextExpiration, purgeNeighborsTimer);
+        else {
+            if (purgeNeighborsTimer->getArrivalTime() != nextExpiration) {
+                cancelEvent(purgeNeighborsTimer);
+                scheduleAt(nextExpiration, purgeNeighborsTimer);
+            }
+        }
+    }
+}
+
+void GPSR::processPurgeNeighborsTimer() {
+    GPSR_EV << "Processing purge neighbors timer" << endl;
+    purgeNeighbors();
+    schedulePurgeNeighborsTimer();
+}
+
+//
+// handling UDP packets
+//
+
+void GPSR::sendUDPPacket(UDPPacket * packet, double delay) {
+    if (delay == 0)
+        send(packet, "ipOut");
+    else
+        sendDelayed(packet, delay, "ipOut");
+}
+
+void GPSR::processUDPPacket(UDPPacket * packet) {
+    cPacket * encapsulatedPacket = packet->decapsulate();
+    if (dynamic_cast<GPSRBeacon *>(encapsulatedPacket)) {
+        GPSRBeacon * beacon = (GPSRBeacon *)encapsulatedPacket;
+        beacon->setControlInfo(packet->removeControlInfo());
+        processBeaconPacket(beacon);
+    }
+    else
+        throw cRuntimeError("Unknown UDP packet");
+    delete packet;
+}
+
+//
+// handling beacons
+//
+
+GPSRBeacon * GPSR::createBeaconPacket() {
+    GPSRBeacon * beacon = new GPSRBeacon();
+    beacon->setAddress(getSelfAddress());
+    beacon->setPosition(mobility->getCurrentPosition());
+    return beacon;
+}
+
+void GPSR::sendBeaconPacket(GPSRBeacon * beacon, double delay) {
+    GPSR_EV << "Sending beacon packet: address = " << beacon->getAddress() << " position = " << beacon->getPosition() << endl;
+    INetworkProtocolControlInfo * networkProtocolControlInfo = addressPolicy->createNetworkProtocolControlInfo();
+    networkProtocolControlInfo->setProtocol(IP_PROT_MANET);
+    networkProtocolControlInfo->setHopLimit(255);
+    networkProtocolControlInfo->setDestinationAddress(addressPolicy->getLinkLocalManetRoutersMulticastAddress());
+    networkProtocolControlInfo->setSourceAddress(getSelfAddress());
+    UDPPacket * udpPacket = new UDPPacket(beacon->getName());
+    udpPacket->encapsulate(beacon);
+    // In its default mode of operation, AODVv2 uses the UDP port 269 [RFC5498] to carry protocol packets.
+    udpPacket->setSourcePort(GPSR_UDP_PORT);
+    udpPacket->setDestinationPort(GPSR_UDP_PORT);
+    udpPacket->setControlInfo(dynamic_cast<cObject *>(networkProtocolControlInfo));
+    sendUDPPacket(udpPacket, delay);
+}
+
+void GPSR::processBeaconPacket(GPSRBeacon * beacon) {
+    GPSR_EV << "Processing beacon packet: address = " << beacon->getAddress() << " position = " << beacon->getPosition() << endl;
+    neighborPositionTable.setPosition(beacon->getAddress(), beacon->getPosition());
+    delete beacon;
+}
+
+//
+// position
+//
+
+Coord GPSR::getDestinationPosition(const Address & address) {
+    // KLUDGE: implement registry protocol
+    return globalPositionTable.getPosition(address);
+}
+
+Coord GPSR::getNeighborPosition(const Address & address) {
+    return neighborPositionTable.getPosition(address);
+}
+
+//
+// address
 //
 
 std::string GPSR::getHostName() {
@@ -75,33 +236,78 @@ std::string GPSR::getHostName() {
 }
 
 Address GPSR::getSelfAddress() {
-    return Address(); // TODO: routingTable->getRouterId();
+    return routingTable->getRouterId();
 }
 
+//
+// neighbor
+//
+
+simtime_t GPSR::getNextNeighborExpiration() {
+    simtime_t oldestPosition = neighborPositionTable.getOldestPosition();
+    if (oldestPosition == SimTime::getMaxTime())
+        return oldestPosition;
+    else
+        return oldestPosition + neighborValidityInterval;
+}
+
+void GPSR::purgeNeighbors() {
+    neighborPositionTable.removeOldPositions(simTime() - neighborValidityInterval);
+}
 
 //
-// generic network protocol
+// next hop
+//
+
+Address GPSR::findNextHop(const Address & destination) {
+    Address bestNeighbor;
+    Coord selfPosition = mobility->getCurrentPosition();
+    Coord destinationPosition = getDestinationPosition(destination);
+    double bestDistance = (destinationPosition - selfPosition).length();
+    std::vector<Address> addresses = neighborPositionTable.getAddresses();
+    for (std::vector<Address>::iterator it = addresses.begin(); it != addresses.end(); it++) {
+        const Address & neighborAddress = *it;
+        Coord neighborPosition = neighborPositionTable.getPosition(neighborAddress);
+        double neighborDistance = (destinationPosition - neighborPosition).length();
+        if (neighborDistance < bestDistance) {
+            bestDistance = neighborDistance;
+            bestNeighbor = neighborAddress;
+        }
+    }
+    return bestNeighbor;
+}
+
+//
+// netfilter
 //
 
 bool GPSR::isGPSRDatagram(INetworkDatagram * datagram) {
     cPacket * packet = dynamic_cast<cPacket *>(datagram);
     INetworkProtocolControlInfo * networkProtocolControlInfo = dynamic_cast<INetworkProtocolControlInfo *>(packet->getControlInfo());
-    return false; //return networkProtocolControlInfo && networkProtocolControlInfo->getProtocol() == IP_PROT_MANET;
+    return networkProtocolControlInfo && networkProtocolControlInfo->getProtocol() == IP_PROT_MANET;
 }
 
-INetfilter::IHook::Result GPSR::ensureRouteForDatagram(INetworkDatagram * datagram) {
+INetfilter::IHook::Result GPSR::routeDatagram(INetworkDatagram * datagram, const InterfaceEntry *& outputInterfaceEntry, Address & nextHop) {
     const Address & source = datagram->getSourceAddress();
     const Address & destination = datagram->getDestinationAddress();
-//    if (destination.isMulticast() || destination.isBroadcast() || routingTable->isLocalAddress(destination) || isGPSRDatagram(datagram))
+    if (destination.isMulticast() || destination.isBroadcast() || routingTable->isLocalAddress(destination) || isGPSRDatagram(datagram))
         return ACCEPT;
-//    else {
-//        GPSR_EV << "Finding route: source = " << source << ", destination = " << destination << endl;
-//        return ACCEPT;
-//    }
+    else {
+        GPSR_EV << "Finding next hop: source = " << source << ", destination = " << destination << endl;
+        nextHop = findNextHop(destination);
+        if (nextHop.isUnspecified())
+            GPSR_EV << "No  next hop found: source = " << source << ", destination = " << destination << endl;
+        else {
+            GPSR_EV << "Next hop found: source = " << source << ", destination = " << destination << ", nextHop: " << nextHop << endl;
+            // KLUDGE:
+            outputInterfaceEntry = interfaceTable->getInterface(1);
+        }
+        return ACCEPT;
+    }
 }
 
 //
-// notifications
+// notification
 //
 
 void GPSR::receiveChangeNotification(int category, const cObject *details) {
--- src/networklayer/routing/gpsr/GPSR.h
+++ src/networklayer/routing/gpsr/GPSR.h
@@ -1,6 +1,20 @@
 //
-// This program is property of its copyright holder. All rights reserved.
-// 
+// Copyright (C) 2004 Andras Varga
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
 
 #ifndef __INET_GPSR_H_
 #define __INET_GPSR_H_
@@ -8,24 +22,40 @@
 #include "INETDefs.h"
 #include "Coord.h"
 #include "INotifiable.h"
+#include "IMobility.h"
 #include "IAddressPolicy.h"
 #include "INetfilter.h"
+#include "IRoutingTable.h"
 #include "NotificationBoard.h"
-#include "LocationTable.h"
+#include "PositionTable.h"
+#include "UDPPacket.h"
+#include "GPSR_m.h"
 
 class INET_API GPSR : public cSimpleModule, public INotifiable, public INetfilter::IHook {
     private:
         // context parameters
+        const char * routingTableModuleName; // TODO: delete when necessary functions are moved to interface table
         const char * networkProtocolModuleName;
+
+        // gpsr parameters
+        const char * interfaces;
+        simtime_t beaconInterval;
+        simtime_t maxJitter;
+        simtime_t neighborValidityInterval;
 
         // context
         NotificationBoard * notificationBoard;
+        IMobility * mobility;
         IAddressPolicy * addressPolicy;
+        IInterfaceTable * interfaceTable;
+        IRoutingTable * routingTable; // TODO: delete when necessary functions are moved to interface table
         INetfilter * networkProtocol;
-        LocationTable * globalLocationTable; // TODO: use a protocol message for lookup?
+        static PositionTable globalPositionTable; // KLUDGE: TODO: implement registry protocol
 
         // internal
-        LocationTable neighborLocationTable;
+        cMessage * beaconTimer;
+        cMessage * purgeNeighborsTimer;
+        PositionTable neighborPositionTable;
 
     public:
         GPSR();
@@ -42,24 +72,48 @@ class INET_API GPSR : public cSimpleModule, public INotifiable, public INetfilte
         void processSelfMessage(cMessage * message);
         void processMessage(cMessage * message);
 
-        // handling positions
-        Coord getLocation(const Address & address);
-        Coord findNextHop(const Address & destination);
+        // handling beacon timers
+        void scheduleBeaconTimer();
+        void processBeaconTimer();
 
-        // handling addresses
+        // handling purge neighbors timers
+        void schedulePurgeNeighborsTimer();
+        void processPurgeNeighborsTimer();
+
+        // handling UDP packets
+        void sendUDPPacket(UDPPacket * packet, double delay);
+        void processUDPPacket(UDPPacket * packet);
+
+        // handling beacons
+        GPSRBeacon * createBeaconPacket();
+        void sendBeaconPacket(GPSRBeacon * beacon, double delay);
+        void processBeaconPacket(GPSRBeacon * beacon);
+
+        // position
+        Coord getDestinationPosition(const Address & address);
+        Coord getNeighborPosition(const Address & address);
+
+        // address
         std::string getHostName();
         Address getSelfAddress();
 
-        // generic network protocol
-        virtual Result datagramPreRoutingHook(INetworkDatagram * datagram, const InterfaceEntry * inputInterfaceEntry) { Enter_Method("datagramPreRoutingHook"); return ensureRouteForDatagram(datagram); }
-        virtual Result datagramLocalInHook(INetworkDatagram * datagram, const InterfaceEntry * inputInterfaceEntry) { return ACCEPT; }
-        virtual Result datagramForwardHook(INetworkDatagram * datagram, const InterfaceEntry * inputInterfaceEntry, const InterfaceEntry *& outputInterfaceEntry, Address & nextHopAddress) { return ACCEPT; }
-        virtual Result datagramPostRoutingHook(INetworkDatagram * datagram, const InterfaceEntry * inputInterfaceEntry, const InterfaceEntry *& outputInterfaceEntry, Address & nextHopAddress) { return ACCEPT; }
-        virtual Result datagramLocalOutHook(INetworkDatagram * datagram, const InterfaceEntry *& outputInterfaceEntry) { Enter_Method("datagramLocalOutHook"); return ensureRouteForDatagram(datagram); }
-        bool isGPSRDatagram(INetworkDatagram * datagram);
-        Result ensureRouteForDatagram(INetworkDatagram * datagram);
+        // neighbor
+        simtime_t getNextNeighborExpiration();
+        void purgeNeighbors();
 
-        // notifications
+        // next hop
+        Address findNextHop(const Address & destination);
+
+        // netfilter
+        virtual Result datagramPreRoutingHook(INetworkDatagram * datagram, const InterfaceEntry * inputInterfaceEntry, const InterfaceEntry *& outputInterfaceEntry, Address & nextHop) { return routeDatagram(datagram, outputInterfaceEntry, nextHop); }
+        virtual Result datagramForwardHook(INetworkDatagram * datagram, const InterfaceEntry * inputInterfaceEntry, const InterfaceEntry *& outputInterfaceEntry, Address & nextHop) { return ACCEPT; }
+        virtual Result datagramPostRoutingHook(INetworkDatagram * datagram, const InterfaceEntry * inputInterfaceEntry, const InterfaceEntry *& outputInterfaceEntry, Address & nextHop) { return ACCEPT; }
+        virtual Result datagramLocalInHook(INetworkDatagram * datagram, const InterfaceEntry * inputInterfaceEntry) { return ACCEPT; }
+        virtual Result datagramLocalOutHook(INetworkDatagram * datagram, const InterfaceEntry *& outputInterfaceEntry, Address & nextHop) { return routeDatagram(datagram, outputInterfaceEntry, nextHop); }
+        bool isGPSRDatagram(INetworkDatagram * datagram);
+        Result routeDatagram(INetworkDatagram * datagram, const InterfaceEntry *& outputInterfaceEntry, Address & nextHop);
+
+        // notification
         virtual void receiveChangeNotification(int category, const cObject * details);
 };
 
